d37631f5661c4d4817d7853f9695e76797918cca
liushuhao <liushuhao@sina.com>
2023-02-12 22:54:36 +0800

    feat: 22

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,23 +4,24 @@
  * @Autor: liushuhao
  * @Date: 2023-02-10 09:58:28
  * @LastEditors: liushuhao
- * @LastEditTime: 2023-02-10 17:15:00
+ * @LastEditTime: 2023-02-12 13:18:58
 -->
 <template>
   <div class="home">
     <span>homehomehomehomehomehomehome</span>
     <p>homehomehomehomehomehomehomehomehomehomehomehomehomehomehomehomehomehomehomehomehome</p>
+    <button @click="go">选择</button>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { useRouter } from 'vue-router';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+const router = useRouter();
+
+const go = () => {
+  router.push({ path: 'user' });
+};
 </script>
