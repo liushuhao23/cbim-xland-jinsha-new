ce34620e8ac1205b847a660527088a1950656519
liushuhao <liushuhao@sina.com>
2023-02-19 00:34:24 +0800

    sss

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Description: description
+ * @Version: 2.0
+ * @Autor: liushuhao
+ * @Date: 2023-02-11 22:28:33
+ * @LastEditors: liushuhao
+ * @LastEditTime: 2023-02-17 21:16:51
+ */
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
@@ -17,8 +25,9 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/user',
     name: 'User',
-    component: User,
+    component: import(/* webpackChunkName: 'User' */ '../views/User.vue'),
   },
+
   // {
   //   path: '/about',
   //   component: About,
